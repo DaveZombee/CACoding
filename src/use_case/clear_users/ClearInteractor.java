5c0b6ebf14865dbe74c39a04537fbd558a7583d3
DaveZombee <20luky301@gmail.com>
2023-10-14 01:14:08 -0400

    Create variables and methods for ClearInteractor

--- src/use_case/clear_users/ClearInteractor.java
+++ src/use_case/clear_users/ClearInteractor.java
@@ -2,5 +2,22 @@ package use_case.clear_users;
 
 // TODO Complete me
 
-public class ClearInteractor {
+public class ClearInteractor implements ClearInputBoundary {
+
+    final ClearUserDataAccessInterface clearDataAccessObject;
+
+    final ClearOutputBoundary clearPresenter;
+
+    public ClearInteractor(ClearUserDataAccessInterface clearDataAccessInterface,
+                           ClearOutputBoundary clearOutputBoundary
+                           ) {
+
+        this.clearDataAccessObject = clearDataAccessInterface;
+        this.clearPresenter = clearOutputBoundary;
+    }
+
+    @Override
+    public void execute(ClearInputData clearInputData) {
+
+    }
 }
